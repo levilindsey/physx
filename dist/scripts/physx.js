f28cf3efe1100f5dcc37569b42af705c21528f7c
Levi Lindsey <levi@levi.codes>
2018-08-11 22:34:39 +0000

    Update deps

--- dist/scripts/physx.js
+++ dist/scripts/physx.js
@@ -5710,26 +5710,26 @@ exports.PhysicsJob = PhysicsJob;
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 exports.PhysicsState = undefined;
 
 var _createClass = function () {
-    function defineProperties(target, props) {
-        for (var i = 0; i < props.length; i++) {
-            var descriptor = props[i];descriptor.enumerable = descriptor.enumerable || false;descriptor.configurable = true;if ("value" in descriptor) descriptor.writable = true;Object.defineProperty(target, descriptor.key, descriptor);
-        }
-    }return function (Constructor, protoProps, staticProps) {
-        if (protoProps) defineProperties(Constructor.prototype, protoProps);if (staticProps) defineProperties(Constructor, staticProps);return Constructor;
-    };
+  function defineProperties(target, props) {
+    for (var i = 0; i < props.length; i++) {
+      var descriptor = props[i];descriptor.enumerable = descriptor.enumerable || false;descriptor.configurable = true;if ("value" in descriptor) descriptor.writable = true;Object.defineProperty(target, descriptor.key, descriptor);
+    }
+  }return function (Constructor, protoProps, staticProps) {
+    if (protoProps) defineProperties(Constructor.prototype, protoProps);if (staticProps) defineProperties(Constructor, staticProps);return Constructor;
+  };
 }();
 
 var _util = require('../util');
 
 function _classCallCheck(instance, Constructor) {
-    if (!(instance instanceof Constructor)) {
-        throw new TypeError("Cannot call a class as a function");
-    }
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError("Cannot call a class as a function");
+  }
 }
 
 /**
@@ -5737,106 +5737,106 @@ function _classCallCheck(instance, Constructor) {
  * position, momentum, and mass).
  */
 var PhysicsState = function () {
-    /**
-     * @param {DynamicsConfig} [dynamicsParams={}]
-     */
-    function PhysicsState() {
-        var dynamicsParams = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+  /**
+   * @param {DynamicsConfig} [dynamicsParams={}]
+   */
+  function PhysicsState() {
+    var dynamicsParams = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
 
-        _classCallCheck(this, PhysicsState);
+    _classCallCheck(this, PhysicsState);
 
-        var position = dynamicsParams.position || vec3.create();
-        var momentum = dynamicsParams.momentum || vec3.create();
-        var orientation = dynamicsParams.orientation || quat.create();
-        var angularMomentum = dynamicsParams.angularMomentum || vec3.create();
-        var mass = dynamicsParams.mass || 1;
-        var unrotatedInertiaTensor = dynamicsParams.unrotatedInertiaTensor || (0, _util.createBoxInertiaTensor)(1, 1, 1, mass);
+    var position = dynamicsParams.position || vec3.create();
+    var momentum = dynamicsParams.momentum || vec3.create();
+    var orientation = dynamicsParams.orientation || quat.create();
+    var angularMomentum = dynamicsParams.angularMomentum || vec3.create();
+    var mass = dynamicsParams.mass || 1;
+    var unrotatedInertiaTensor = dynamicsParams.unrotatedInertiaTensor || (0, _util.createBoxInertiaTensor)(1, 1, 1, mass);
 
-        // Constant fields.
+    // Constant fields.
 
-        this.mass = mass;
-        this.inverseMass = 1 / mass;
-        this.unrotatedInertiaTensor = unrotatedInertiaTensor;
+    this.mass = mass;
+    this.inverseMass = 1 / mass;
+    this.unrotatedInertiaTensor = unrotatedInertiaTensor;
 
-        // Independent fields.
+    // Independent fields.
 
-        this.position = position;
-        this.momentum = momentum;
-        this.orientation = orientation;
-        this.angularMomentum = angularMomentum;
+    this.position = position;
+    this.momentum = momentum;
+    this.orientation = orientation;
+    this.angularMomentum = angularMomentum;
 
-        // Dependent fields.
+    // Dependent fields.
 
-        // Linear velocity.
-        this.velocity = vec3.create();
-        // Quaternion-based representation of the rate of change in orientation.
-        this.spin = quat.create();
-        // Vector-based representation of the angular velocity.
-        this.angularVelocity = vec3.create();
-        // The inverse inertia tensor rotated to the world coordinate frame.
-        this.inverseInertiaTensor = mat3.create();
+    // Linear velocity.
+    this.velocity = vec3.create();
+    // Quaternion-based representation of the rate of change in orientation.
+    this.spin = quat.create();
+    // Vector-based representation of the angular velocity.
+    this.angularVelocity = vec3.create();
+    // The inverse inertia tensor rotated to the world coordinate frame.
+    this.inverseInertiaTensor = mat3.create();
 
-        this.updateDependentFields();
+    this.updateDependentFields();
+  }
+
+  _createClass(PhysicsState, [{
+    key: 'updateDependentFields',
+    value: function updateDependentFields() {
+      // TODO: Test this somehow...
+      // Update linear velocity.
+      vec3.scale(this.velocity, this.momentum, this.inverseMass);
+
+      // Update angular velocity.
+      quat.normalize(this.orientation, this.orientation);
+      (0, _util.rotateTensor)(this.inverseInertiaTensor, this.unrotatedInertiaTensor, this.orientation);
+      mat3.invert(this.inverseInertiaTensor, this.unrotatedInertiaTensor);
+      vec3.transformMat3(this.angularVelocity, this.angularMomentum, this.inverseInertiaTensor);
+      quat.set(this.spin, this.angularVelocity[0], this.angularVelocity[1], this.angularVelocity[2], 0);
+      quat.scale(this.spin, this.spin, 0.5);
+      quat.multiply(this.spin, this.spin, this.orientation);
     }
 
-    _createClass(PhysicsState, [{
-        key: 'updateDependentFields',
-        value: function updateDependentFields() {
-            // TODO: Test this somehow...
-            // Update linear velocity.
-            vec3.scale(this.velocity, this.momentum, this.inverseMass);
+    /**
+     * Perform a deep copy.
+     *
+     * @param {PhysicsState} other
+     */
 
-            // Update angular velocity.
-            quat.normalize(this.orientation, this.orientation);
-            (0, _util.rotateTensor)(this.inverseInertiaTensor, this.unrotatedInertiaTensor, this.orientation);
-            mat3.invert(this.inverseInertiaTensor, this.unrotatedInertiaTensor);
-            vec3.transformMat3(this.angularVelocity, this.angularMomentum, this.inverseInertiaTensor);
-            quat.set(this.spin, this.angularVelocity[0], this.angularVelocity[1], this.angularVelocity[2], 0);
-            quat.scale(this.spin, this.spin, 0.5);
-            quat.multiply(this.spin, this.spin, this.orientation);
-        }
+  }, {
+    key: 'copy',
+    value: function copy(other) {
+      this.mass = other.mass;
+      this.inverseMass = other.inverseMass;
+      mat3.copy(this.unrotatedInertiaTensor, other.unrotatedInertiaTensor);
+      mat3.copy(this.inverseInertiaTensor, other.inverseInertiaTensor);
+      vec3.copy(this.position, other.position);
+      vec3.copy(this.momentum, other.momentum);
+      quat.copy(this.orientation, other.orientation);
+      vec3.copy(this.angularMomentum, other.angularMomentum);
+      vec3.copy(this.velocity, other.velocity);
+      quat.copy(this.spin, other.spin);
+      vec3.copy(this.angularVelocity, other.angularVelocity);
+    }
 
-        /**
-         * Perform a deep copy.
-         *
-         * @param {PhysicsState} other
-         */
+    /**
+     * @param {PhysicsState} a
+     * @param {PhysicsState} b
+     * @param {number} partialRatio
+     */
 
-    }, {
-        key: 'copy',
-        value: function copy(other) {
-            this.mass = other.mass;
-            this.inverseMass = other.inverseMass;
-            mat3.copy(this.unrotatedInertiaTensor, other.unrotatedInertiaTensor);
-            mat3.copy(this.inverseInertiaTensor, other.inverseInertiaTensor);
-            vec3.copy(this.position, other.position);
-            vec3.copy(this.momentum, other.momentum);
-            quat.copy(this.orientation, other.orientation);
-            vec3.copy(this.angularMomentum, other.angularMomentum);
-            vec3.copy(this.velocity, other.velocity);
-            quat.copy(this.spin, other.spin);
-            vec3.copy(this.angularVelocity, other.angularVelocity);
-        }
+  }, {
+    key: 'lerp',
+    value: function lerp(a, b, partialRatio) {
+      vec3.lerp(this.position, a.position, b.position, partialRatio);
+      vec3.lerp(this.momentum, a.momentum, b.momentum, partialRatio);
+      quat.slerp(this.orientation, a.orientation, b.orientation, partialRatio);
+      quat.normalize(this.orientation, this.orientation);
+      vec3.lerp(this.angularMomentum, a.angularMomentum, b.angularMomentum, partialRatio);
+      this.updateDependentFields();
+    }
+  }]);
 
-        /**
-         * @param {PhysicsState} a
-         * @param {PhysicsState} b
-         * @param {number} partialRatio
-         */
-
-    }, {
-        key: 'lerp',
-        value: function lerp(a, b, partialRatio) {
-            vec3.lerp(this.position, a.position, b.position, partialRatio);
-            vec3.lerp(this.momentum, a.momentum, b.momentum, partialRatio);
-            quat.slerp(this.orientation, a.orientation, b.orientation, partialRatio);
-            quat.normalize(this.orientation, this.orientation);
-            vec3.lerp(this.angularMomentum, a.angularMomentum, b.angularMomentum, partialRatio);
-            this.updateDependentFields();
-        }
-    }]);
-
-    return PhysicsState;
+  return PhysicsState;
 }();
 
 exports.PhysicsState = PhysicsState;
